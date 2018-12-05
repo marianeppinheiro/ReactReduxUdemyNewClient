b26306249a67b456b8ef6069fd324960289ef710
Mariane <mahrinoa@gmail.com>
2018-12-05 22:41:36 +0100

    add redux form and thunk

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -1,18 +1,28 @@
-import React, { Component } from "react";
-
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+import { compose } from 'redux';
 class Signup extends Component {
-  render() {
-    return (
-      <form>
-        <fieldset>
-          <label>Email</label>
-        </fieldset>
-        <fieldset>
-          <label>Password</label>
-        </fieldset>
-      </form>
-    );
-  }
+	onSubmit = (formProps) => {
+		this.props.Signup(formProps);
+	};
+	render() {
+		const { handleSubmit } = this.props;
+		return (
+			<form onSubmit={handleSubmit(this.onSubmit)}>
+				<fieldset>
+					<label>Email</label>
+					<Field name="email" type="text" component="input" autoComplete="none" />
+				</fieldset>
+				<fieldset>
+					<label>Password</label>
+					<Field name="password" type="password" component="input" autoComplete="none" />
+				</fieldset>
+				<button>Sign up!</button>
+			</form>
+		);
+	}
 }
 
-export default Signup;
+export default compose(connect(null, actions), reduxForm({ form: 'signup' }))(Signup);
--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter, Route } from "react-router-dom";
-import App from "./components/App";
-import Welcome from "./components/Welcome";
-import Signup from "./components/auth/Signup";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
+import reducers from './reducers';
+import App from './components/App';
+import Welcome from './components/Welcome';
+import Signup from './components/auth/Signup';
+
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App>
-      <Route path="/" exact component={Welcome} />
-      <Route path="/signup" exact component={Signup} />
-    </App>
-  </BrowserRouter>,
-  document.querySelector("#root")
+	<Provider store={store}>
+		<BrowserRouter>
+			<App>
+				<Route path="/" exact component={Welcome} />
+				<Route path="/signup" exact component={Signup} />
+			</App>
+		</BrowserRouter>
+	</Provider>,
+	document.querySelector('#root')
 );
