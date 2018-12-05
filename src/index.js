a0869015a1bc65684ed39b3d6d7a4f78c99375b9
Mariane Pires Pinheiro <mariane.pires@exact.com>
2018-12-05 11:21:15 +0100

    add router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { BrowserRouter, Route } from "react-router-dom";
 import App from "./components/App";
+import Welcome from "./components/Welcome";
+import Signup from "./components/auth/Signup";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <App>
+      <Route path="/" exact component={Welcome} />
+      <Route path="/signup" exact component={Signup} />
+    </App>
+  </BrowserRouter>,
+  document.querySelector("#root")
+);
